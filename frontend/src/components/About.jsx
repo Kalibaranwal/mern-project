525efad48b3e00105d5930e071460a0a70abff5a
Kalibaranwal <kalibarnwal@gmail.com>
2025-05-30 23:20:31 +0530

    Initial commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+        Welcome to your ultimate event planning companion — a one-stop platform designed to turn your special moments into unforgettable experiences. Whether you're organizing a joyful birthday celebration, a romantic anniversary surprise, an adventurous camping trip, or an epic party, our website helps you plan it all with ease and elegance. From checklists and theme ideas to guest coordination and vendor recommendations, we provide the tools and inspiration to make your vision a reality. With a sleek interface and intuitive features, planning your next event becomes not just stress-free, but exciting. Let us help you celebrate life’s most important occasions — beautifully, effortlessly, and your way.
+      </p>
+    </section>
+  );
+};
+export default About; 
